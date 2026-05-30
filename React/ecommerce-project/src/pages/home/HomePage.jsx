94e6c6135f9e1f363ff3b0842ef635847d6ec080
RohanSachdeva-ops <rohansachdeva1126dp@gmail.com>
2026-05-31 02:06:35 +0530

    convert home and checkout to async await

--- React/ecommerce-project/src/pages/home/HomePage.jsx
+++ React/ecommerce-project/src/pages/home/HomePage.jsx
@@ -10,13 +10,19 @@ export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/products')
-      .then((response) => {
-        setProducts(response.data);
-      });
-
-
+    const getHomeData = async () => {
+    const response = await axios.get('/api/products')
+      setProducts(response.data);
+    };
+    getHomeData();
   }, []);
+
+  // useEffect(() => {
+  //   axios.get('/api/products')
+  //     .then((response) => {
+  //       setProducts(response.data);
+  //     });
+  // }, []);
 
   return (
     <>
